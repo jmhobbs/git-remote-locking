b097691de5e44687d110c532402d5ae3ac5bbe61
John Hobbs <john@velvetcache.org>
2017-09-19 15:11:55 -0500

    Lock and unlock.

---
+++ git-unlock-remote/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("usage: git lock <remote>")
+	}
+
+	out, err := exec.Command("git", "config", "--unset", fmt.Sprintf("remote.%s.pushurl", os.Args[1])).Output()
+	if err != nil {
+		stderr := string(err.(*exec.ExitError).Stderr)
+		if len(stderr) != 0 {
+			log.Fatal(stderr)
+		}
+		log.Fatal("error unlocking remote '", os.Args[1], "' - may not exist?")
+	}
+
+	fmt.Printf("unlocked remote '%s'\n", os.Args[1])
+	if len(out) > 1 {
+		fmt.Println(string(out))
+	}
+}
